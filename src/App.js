4318b28f7bb0a41f867e685327d3304f4832a1d6
jesus <chuygs12@gmail.com>
2023-06-23 16:13:40 -0600

    basic structure of router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Shopping from './components/Shopping'
+import Cart from './components/Cart'
+import HomePage from './components/HomePage'
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
+      <Link to='/shopping'>Shopping</Link>
+      <br/>
+      <Link to='/cart'>Cart</Link>
+      <br/>
+      <Link to='/home'>Home</Link>
+
+      <Routes>
+        <Route path='/shopping' element={<Shopping/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Shopping.js
@@ -0,0 +1,7 @@
+const Shopping = () => {
+    return(
+        <h2>Shopping</h2>
+    )
+}
+
+export default Shopping
